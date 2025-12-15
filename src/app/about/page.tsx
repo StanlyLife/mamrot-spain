6811cc540af25b19eaeb07a4291c675856afa457
StanlyLife <stianhave@hotmail.com>
2025-12-15 16:53:09 +0100

    change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -63,7 +63,7 @@ export default function AboutPage() {
         </div>
         <div className="about-hero__inner">
           <h1 id="about-heading" className="about-animate">
-            About Mamrot Car Spa & Detailing Studio
+            About Mamrot Car Spa & Foiling Studio
           </h1>
           <p className="lede about-animate">
             From the bottom to the top - the story of passion, persistence, and
